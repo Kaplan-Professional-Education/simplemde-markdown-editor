766e1b3f1042e511aeed1d76cfa37b4386a43a61
Antonio Rivero <antonio.riveromartnez@kaplan.com>
2017-10-04 15:18:33 -0300

    KPERET-11572: Add Collapse and expand text to Markdown (Old QoD Widget ticket)
    - Add new custom markup to markdown editor, it nows support this sort of things: [+"View More" -"View Less"]Hidden Text[+-]

---
+++ src/js/markdown-it/plugin/inline-expand.js
@@ -0,0 +1,72 @@
+// Process ~subscript~
+
+"use strict";
+
+var EXPAND_OPEN_REGEX = /\[\s*?\+\s*?"([\w?\W?]+?)"\s*?-\s*?"([\w?\W?]+?)"\s*?\]/;
+var EXPAND_CLOSE_REGEX = /\[\s*?\+\s*?-\s*?\]/;
+var TOKEN_TYPE = "expand_collapse";
+var MARKUP = "{expand}";
+
+function expand_collapse(state, silent) {
+	var content,
+		token,
+		startResult,
+		endResult,
+		result,
+		max = state.posMax,
+		start = state.pos;
+
+	if (state.src.charCodeAt(start) !== 0x5B/* ~ */) { return false; }
+	if (silent) { return false; } // don't run any pairs in validation mode
+
+	content = state.src.slice(start);
+	startResult = EXPAND_OPEN_REGEX.exec(content);
+	endResult = EXPAND_CLOSE_REGEX.exec(content);
+
+	if(!startResult && !endResult) { return false; }
+
+	if(startResult && endResult) {
+		if(startResult.index < endResult.index) {
+			result = startResult;
+		} else {
+			result = endResult;
+		}
+	} else if(startResult) {
+		result = startResult;
+	} else {
+		result = endResult;
+	}
+
+	state.posMax = start + result[0].length;
+
+	token = state.push(TOKEN_TYPE, "ExpandA", 0);
+	token.markup = MARKUP;
+	token.tag = "ExpandA";
+	token.props = {};
+
+	token.props.expandText = result[1];
+	token.props.collapseText = result[2];
+	token.props.content = endResult && startResult && content.slice(startResult[0].length, endResult.index);
+    
+	state.src = state.src.replace(new RegExp(token.props.content), "");
+	state.pos = state.posMax;
+	state.posMax = max;
+	return true;
+}
+
+function renderJSX(tokens, idx) {
+	var token = tokens[idx];
+
+	// Note: The following will only render in the CMS - MDReactComponent will replace with the actual component
+	return "<span style='border: 1px dashed #ccc; background-color: #FFFFCE; display: inline-block; margin: 5px; padding: 5px'>"
+		+ token.tag + " Component"
+		+ "</span>";
+}
+
+module.exports = function expand_plugin(md, name, options) {
+	options = options || {};
+
+	md.inline.ruler.before("html_inline", "expand_collapse", expand_collapse);
+
+	md.renderer.rules[TOKEN_TYPE] = options.render || renderJSX;
+};
