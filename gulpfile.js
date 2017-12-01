b296ae9f9496ef33e4e25b94ab035ba2e054921a
Jeffrey Farb <jeffrey.farb@kaplan.com>
2017-12-01 12:34:33 -0800

    Changed all tabs to spaces, changed eslint to match, add 'npm run gulp' script for easier use without needing global glup, add markdown-it debug file, remove redundant simplemde debug file, rebuild dist and debug files

--- gulpfile.js
+++ gulpfile.js
@@ -1,103 +1,96 @@
 "use strict";
 
 var gulp = require("gulp"),
-  minifycss = require("gulp-clean-css"),
-  uglify = require("gulp-uglify"),
-  concat = require("gulp-concat"),
-  header = require("gulp-header"),
-  buffer = require("vinyl-buffer"),
-  pkg = require("./package.json"),
-  debug = require("gulp-debug"),
-  eslint = require("gulp-eslint"),
-  prettify = require("gulp-jsbeautifier"),
-  browserify = require("browserify"),
-  source = require("vinyl-source-stream"),
-  rename = require("gulp-rename");
+    minifycss = require("gulp-clean-css"),
+    uglify = require("gulp-uglify"),
+    concat = require("gulp-concat"),
+    header = require("gulp-header"),
+    buffer = require("vinyl-buffer"),
+    pkg = require("./package.json"),
+    debug = require("gulp-debug"),
+    eslint = require("gulp-eslint"),
+    prettify = require("gulp-jsbeautifier"),
+    browserify = require("browserify"),
+    source = require("vinyl-source-stream"),
+    rename = require("gulp-rename");
 
 var banner = ["/**",
-  " * <%= pkg.name %> v<%= pkg.version %>",
-  " * Copyright <%= pkg.company %>",
-  " * @link <%= pkg.homepage %>",
-  " * @license <%= pkg.license %>",
-  " */",
-  ""].join("\n");
+    " * <%= pkg.name %> v<%= pkg.version %>",
+    " * Copyright <%= pkg.company %>",
+    " * @link <%= pkg.homepage %>",
+    " * @license <%= pkg.license %>",
+    " */",
+    ""].join("\n");
 
 gulp.task("prettify-css", [], function() {
-  return gulp.src("./src/css/simplemde.css")
-    .pipe(prettify({css: {indentChar: "\t", indentSize: 1}}))
-    .pipe(gulp.dest("./src/css"));
+    return gulp.src("./src/css/simplemde.css")
+        .pipe(prettify({css: {indentChar: "\t", indentSize: 1}}))
+        .pipe(gulp.dest("./src/css"));
 });
 
 gulp.task("lint", [], function() {
-  gulp.src("./src/js/**/*.js")
-    .pipe(debug())
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
+    gulp.src("./src/js/**/*.js")
+        .pipe(debug())
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
 });
 
 function taskBrowserify(opts) {
-  return browserify("./src/js/simplemde.js", opts)
-    .bundle();
+    return browserify("./src/js/simplemde.js", opts)
+        .bundle();
 }
 
-gulp.task("browserify:debug", ["lint"], function() {
-  return taskBrowserify({debug:true, standalone:"SimpleMDE"})
-    .pipe(source("simplemde.debug.js"))
-    .pipe(buffer())
-    .pipe(header(banner, {pkg: pkg}))
-    .pipe(gulp.dest("./debug/"));
-});
-
 gulp.task("browserify", ["lint"], function() {
-  return taskBrowserify({standalone:"SimpleMDE"})
-    .pipe(source("simplemde.js"))
-    .pipe(buffer())
-    .pipe(header(banner, {pkg: pkg}))
-    .pipe(gulp.dest("./debug/"));
+    return taskBrowserify({standalone:"SimpleMDE"})
+        .pipe(source("simplemde.js"))
+        .pipe(buffer())
+        .pipe(header(banner, {pkg: pkg}))
+        .pipe(gulp.dest("./debug/"));
 });
 
-gulp.task("build:simplemde", ["browserify:debug", "browserify", "lint"], function() {
-  var js_files = ["./debug/simplemde.js"];
+gulp.task("build:simplemde", ["browserify", "lint"], function() {
+    var js_files = ["./debug/simplemde.js"];
 
-  return gulp.src(js_files)
-    .pipe(concat("simplemde.min.js"))
-    .pipe(uglify())
-    .pipe(buffer())
-    .pipe(header(banner, {pkg: pkg}))
-    .pipe(gulp.dest("./dist/"));
+    return gulp.src(js_files)
+        .pipe(concat("simplemde.min.js"))
+        .pipe(uglify())
+        .pipe(buffer())
+        .pipe(header(banner, {pkg: pkg}))
+        .pipe(gulp.dest("./dist/"));
 });
 
 gulp.task("build:markdown-it", ["lint"], function() {
-  return browserify("./src/js/markdown-it/index.js", {standalone:"MarkdownIt"})
-    .bundle()
-    .pipe(source("markdown-it.js"))
-    .pipe(buffer())
-    .pipe(rename("markdown-it.min.js"))
-    .pipe(uglify())
-    .pipe(buffer())
-    .pipe(gulp.dest("./dist/"));
+    return browserify("./src/js/markdown-it/index.js", {standalone:"MarkdownIt"})
+        .bundle()
+        .pipe(source("markdown-it.js"))
+        .pipe(buffer())
+        .pipe(gulp.dest("./debug/"))
+        .pipe(rename("markdown-it.min.js"))
+        .pipe(uglify())
+        .pipe(buffer())
+        .pipe(gulp.dest("./dist/"));
 });
 
 gulp.task("scripts", ["build:simplemde", "build:markdown-it"]);
 
 gulp.task("styles", ["prettify-css"], function() {
-  var css_files = [
-    "./node_modules/codemirror/lib/codemirror.css",
-    "./src/css/*.css",
-    "./node_modules/codemirror-spell-checker/src/css/spell-checker.css"
-  ];
+    var css_files = [
+        "./node_modules/codemirror/lib/codemirror.css",
+        "./src/css/*.css",
+        "./node_modules/codemirror-spell-checker/src/css/spell-checker.css"
+    ];
 
-  return gulp.src(css_files)
-    .pipe(concat("simplemde.css"))
-    .pipe(buffer())
-    .pipe(header(banner, {pkg: pkg}))
-    .pipe(gulp.dest("./debug/"))
-    .pipe(minifycss())
-    .pipe(rename("simplemde.min.css"))
-    .pipe(buffer())
-    .pipe(header(banner, {pkg: pkg}))
-    .pipe(gulp.dest("./dist/"));
+    return gulp.src(css_files)
+        .pipe(concat("simplemde.css"))
+        .pipe(buffer())
+        .pipe(header(banner, {pkg: pkg}))
+        .pipe(gulp.dest("./debug/"))
+        .pipe(minifycss())
+        .pipe(rename("simplemde.min.css"))
+        .pipe(buffer())
+        .pipe(header(banner, {pkg: pkg}))
+        .pipe(gulp.dest("./dist/"));
 });
 
 gulp.task("default", ["scripts", "styles"]);
--- src/js/markdown-it/index.js
+++ src/js/markdown-it/index.js
@@ -1,12 +1,12 @@
 module.exports = {
-	Renderer: require("markdown-it"),
-	Plugins: [
-		require("markdown-it-sup"),
-		require("./plugin/inline-text-color"),
-		require("./plugin/block-align"),
-		require("./plugin/inline-jsx"),
-		require("./plugin/inline-icons")(["fa", "kf"]),
-		require("./plugin/inline-link-targets"),
-		require("./plugin/block-expand"),
-	],
+    Renderer: require("markdown-it"),
+    Plugins: [
+        require("markdown-it-sup"),
+        require("./plugin/inline-text-color"),
+        require("./plugin/block-align"),
+        require("./plugin/inline-jsx"),
+        require("./plugin/inline-icons")(["fa", "kf"]),
+        require("./plugin/inline-link-targets"),
+        require("./plugin/block-expand"),
+    ],
 };
