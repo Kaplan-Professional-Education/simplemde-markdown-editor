cd84a4d57eace297db37489efef1cd003c22d7e1
Jeffrey Farb <jeffrey.farb@kaplan.com>
2018-01-03 13:55:28 -0800

    Simplify plugin

--- src/js/markdown-it/plugin/inline-dynamic-modal.js
+++ src/js/markdown-it/plugin/inline-dynamic-modal.js
@@ -3,54 +3,24 @@
 
 "use strict";
 
-var DYNAMIC_OPEN_REGEX = /~!~((?:(?!~!~).)+)/;
-var DYNAMIC_CLOSE_REGEX = /(}\~\!\~){1}/;
+var DYNAMIC_REGEX = /~!~((?:(?!~!~).)+)~!~/;
 var TOKEN_TYPE = "dynamic_content";
-var MARKUP = "{dynamic_content}";
 
 function dynamic_content(state, silent) {
-    var content,
-        token,
-        startResult,
-        endResult,
-        result,
-        nextPos,
-        max = state.posMax,
-        start = state.pos;
+    var match, token, end, pos = state.pos;
 
-    if (state.src.charCodeAt(start) !== 0x7E/* ~ */) { return false; }
-    if (silent) { return false; } // don't run any pairs in validation mode
-    
-    content = state.src.slice(start);
-    startResult = DYNAMIC_OPEN_REGEX.exec(content);
-    endResult = DYNAMIC_CLOSE_REGEX.exec(content);
-    if(!startResult && !endResult) { return false; }
-    if(startResult && endResult) {
-        if(startResult.index < endResult.index) {
-            result = startResult[1];
-        } else {
-            result = endResult;
-        }
-        nextPos = endResult.index + 4;
-    } else if(startResult) {
-        result = startResult[1];
-        nextPos = startResult[0].length;
-    } else {
-        result = endResult;
-        nextPos = endResult.index + 4;
+    match = state.src.slice(pos).match(DYNAMIC_REGEX);
+    if (!match) { return false; }
+
+    end = match[0].length;
+
+    if (!silent) {
+        token = state.push(TOKEN_TYPE, "DynamicContentA", 0);
+        token.props = {};
+        token.props.content = match[1];
+        token.props.rawText = match[0];
     }
-
-    state.posMax = start + nextPos;  
-    token = state.push(TOKEN_TYPE, "DynamicContentA", 0);
-    token.markup = MARKUP;
-    token.tag = "DynamicContentA";
-    token.props = {};
-    token.props.content = result;
-    token.props.rawText = content;
-    
-    state.src = state.src.replace(new RegExp(content), "");
-    state.pos = state.posMax;
-    state.posMax = max;
+    state.pos += end;
     return true;
 }
 
@@ -69,7 +39,7 @@ function renderDynamicContent(tokens, idx) {
         + "</span>";
 }
 
-module.exports = function jsx_plugin(md) {
-    md.inline.ruler.before("html_inline", "dynamic_content", dynamic_content);  
+module.exports = function dynamic_content_plugin(md) {
+    md.inline.ruler.before("html_inline", "dynamic_content", dynamic_content);
     md.renderer.rules["dynamic_content"] = renderDynamicContent;
 };
